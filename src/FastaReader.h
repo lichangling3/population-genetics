bc35ae1233e44846758223932406fe4d54874f52
Gürtler Laurent <lguertle@INTRANET.EPFL.CH@SVUbuUniv8.intranet.epfl.ch>
2019-11-05 19:26:58 +0100

    commit FastaReader 2

--- src/FastaReader.h
+++ src/FastaReader.h
@@ -7,9 +7,7 @@
 class FastaReader
 {
 	public:
-	//two constructors are here provided. You can choose the one you prefer depending on the context and preference
 	FastaReader();	//constructor without arguments
-	FastaReader(std::vector<int> marks, string file_to_read);	//constructor with arguments
 	
 	private:
 	std::vector<int> marks;		//I expect marks given as a vector
