1378cf9de1b67012ad7e5b59f7022a7a444b2070
Mayer Julia Stéphanie <mayer@INTRANET.EPFL.CH@SVUbuUniv11.intranet.epfl.ch>
2019-11-06 16:43:49 +0100

    "addition of test declaration for random" ^O"tests.cpp"

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,13 @@
 #include "FastaReader.h"
+#incude "random.h"
 
 TEST(readFileTest, readFile) {
 	FastaReader s1({1,3,6}, "file.txt");
 	//Write the rest of the test below
+}
+
+TEST(Random, multinomial){
+	RandomNumbers rng;
+	//write rest of the test
+	
 }
