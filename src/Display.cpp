aa87c5ee7afd2e6b3504ee6140e92f739d42635b
Lissane Eddine Fatima Zahra <lissane@INTRANET.EPFL.CH@SVUbuUniv25.intranet.epfl.ch>
2019-11-15 13:13:27 +0100

    appel de simulation

--- src/Display.cpp
+++ src/Display.cpp
@@ -1,27 +1,28 @@
 #include "Display.h"
+#include "Simulation.h"
 #include <iomanip>
 
 Display::Display(){}
 
-void displayGen( Population pop ){ 
+void displayGen(Population pop , size_t time){ 
 	
-Alleles alleles = pop.getpopAlleles();
 
 std::ofstream write;
 write.open("Team_1/display.txt"); //will be created if it doesn't exist
 
 if(write){	//did it open well? 
 	
-	for (int i(0); i < 5 ; ++i){ //LE SIMULATION PAS ENCORE CREE donc mettre du style Simulation.nombre_generations_total au lieu de 5 (c'était juste pour que ca compile)
+	for (int i(0); i < time ; ++i){ //LE SIMULATION PAS ENCORE CREE donc mettre du style Simulation.nombre_generations_total au lieu de 5 (c'était juste pour que ca compile)
 		write << i <<  std::setw(5);
 	
-		for ( Alleles::iterator it = alleles.begin(); it !=alleles.end(); ++it){
+		for ( Alleles::iterator it = pop.getpopAlleles().begin(); it !=pop.getpopAlleles().end(); ++it){
 			write << it->second<<"|";
 		}
+		_SIM -> update(pop.getFrequencies());
 		write << std::endl;
 	}
 	write << std::setw(5);
-	for(Alleles::iterator it = alleles.begin(); it !=alleles.end(); ++it){		
+	for(Alleles::iterator it = pop.getpopAlleles().begin(); it !=pop.getpopAlleles().end(); ++it){		
 		write << it->first<<"|"; 
 		}
 
