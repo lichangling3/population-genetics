935e5a5972eb1c08b3ab09ad9ec54acd2db380d0
Changling Li <changling.li@epfl.ch>
2019-11-15 11:42:15 +0100

    TCLAP in main.cpp works, still need to make it beautiful

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,83 @@
 #include <vector>
 #include <string>
 #include <tclap/CmdLine.h>
-
+#include <cmath>
+#include "FastaReader.h"
 
 void parse_args(int argc, char **argv) 
 {
 	TCLAP::CmdLine cmd("Population definition");
 
-    TCLAP::ValueArg<size_t> duration("T", "time", "Simulation's duration", false, 10, "size_t");
-    cmd.add(duration);
-    TCLAP::ValueArg<size_t> repeat("R", "repeat", "Number of repetitions of simulation", false, 2, "size_t");
-    cmd.add(repeat);
 	TCLAP::ValueArg<size_t> nsample("N", "population_size", "Number of individuals", false, 100, "size_t");
     cmd.add(nsample);
-    TCLAP::ValueArg<size_t> number_alleles("A", "number_of_alleles", "Number of alleles", false, 2, "size_t");
+	TCLAP::ValueArg<size_t> number_alleles("A", "number_of_alleles", "Number of alleles", false, 2, "size_t");
     cmd.add(number_alleles);
+    TCLAP::ValueArg<int> duration("T", "time", "Simulation's duration", false, 10, "size_t");
+    cmd.add(duration);
+    TCLAP::ValueArg<int> repeat("R", "repeat", "Number of repetitions of simulation", false, 2, "size_t");
+    cmd.add(repeat);
 	TCLAP::ValueArg<std::string> file_name("F", "file_name", "Name of the FASTA file", false, "../tests/test_for_retrieveData.fasta", "string");
     cmd.add(file_name);
-	TCLAP::MultiArg <double> freq("f", "frequences", "Initial frequences of the alleles", false, "double");
-	TCLAP::MultiArg <size_t> marks("m", "marks", "Sequence positions when FASTA file provided", false, "size_t");
+	TCLAP::MultiArg<double> freq("f", "frequences", "Initial frequences of the alleles", false, "double");
+	TCLAP::MultiArg<size_t> marks("m", "marks", "Sequence positions when FASTA file provided", false, "size_t");
 	cmd.xorAdd(marks, freq);
 
     cmd.parse(argc, argv);
     
-    //retrieve values maybe needed for simulation constructor
-    size_t sim_duration = duration.getValue();
-    size_t repetitions = repeat.getValue();
-    size_t nb_alleles = number_alleles.getValue();
-    std::string file = file_name.getValue();
-    std::vector<double> nb_freq = freq.getValue();
-    std::vector<size_t> nb_marks = marks.getValue();
+	if (!duration.isSet()) {
+		throw std::runtime_error("Simulation duration needed");
+	}
+	if (!repeat.isSet()) {
+		throw std::runtime_error("Number of repetitions of simulation needed");
+	}
+	int time(duration.getValue());
+	int repetition(repeat.getValue());
     
 	if (file_name.isSet()) {
 		if (!marks.isSet()) {
 			throw std::runtime_error("Provide marks, frequences not needed");
 		} 
-		if (nsample.isSet()) {
+		else if (nsample.isSet()) {
 			throw std::runtime_error("Population size not needed");
 		}
-		if (freq.isSet()) {
+		else if (freq.isSet()) {
 			throw std::runtime_error("Frequences not needed");
 		}
+		else if (!marks.isSet()) {
+			throw std::runtime_error("Marks needed");
+		}
+		std::map<std::string, double> freq_per_allele(FastaReader::retrieveData(marks.getValue(), file_name.getValue()));
+		int population_size(FastaReader::size(marks.getValue(), file_name.getValue()));
 	} else if (!file_name.isSet()) {
 		if (marks.isSet()) {
-			throw std::runtime_error("Marks not needed");
+			throw std::runtime_error("Provide frequences, marks not needed");
 		}
-		 if (nb_freq.size() < number_alleles.getValue()){
-			 throw std::runtime_error("Initial frequences needed for all alleles");
-		 }
-		 else if (nb_freq.size() > number_alleles.getValue()){
-			 throw std::runtime_error("Number of frequences should be equal to number of alleles");
-		 }
+		else if (!nsample.isSet()) {
+			throw std::runtime_error("Number of individuals needed");
+		}
+		else if (!number_alleles.isSet()) {
+			throw std::runtime_error("Number of alleles needed");
+		}
+		else if (!freq.isSet()) {
+			throw std::runtime_error("Initial frequences of each allele needed");
+		}
+		else if (nsample.getValue() < number_alleles.getValue()) {
+			throw std::runtime_error("There is too much alleles compared to the size of population");
+		}
+		else if (number_alleles.getValue() != (freq.getValue()).size()) {
+			throw std::runtime_error("The number of alleles must be equal to the number of frequences provided");
+		} else {
+			double sum(0);
+			for (size_t i(0); i < (freq.getValue()).size(); ++i) {
+				sum += (freq.getValue())[i];
+			}
+			if (std::abs(sum - 1.0) > 1e-6) {
+				throw std::runtime_error("The sum of frequences must be equal to 1.0");
+			}
+		}
+		int population_size(nsample.getValue());
+		int nb_alleles(number_alleles.getValue());
+		std::vector<double> frequences(freq.getValue());
 	}
 }
 
@@ -59,9 +86,15 @@ int main(int argc, char **argv)
 	int nerr = 0;
 	try {
 		parse_args(argc, argv);
-		
-		
-		//Mettre ici vos fonctions pour initialiser simulation.
+		//Petit résumé des valeurs 
+		// int time
+		// int repetion
+		// std::map<std::string, double> freq_per_allele (lorsque l'utilisateur fournit un fichier FASTA)
+		// int population_size
+		// int nb_alleles
+		// std::vector<double> frequences
+
+		//Mettre ici vos fonctions pour initialiser simulation, etc.
 	} catch(std::runtime_error &e) {
 		std::cerr << e.what() << "\n";
 		nerr = 1;
