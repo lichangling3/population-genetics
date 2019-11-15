ffd901d0d52e1c15e016f91394e2b6c2c3f6c8ea
Lissane Eddine Fatima Zahra <lissane@INTRANET.EPFL.CH@SVUbuUniv25.intranet.epfl.ch>
2019-11-15 13:32:21 +0100

    ajouter getFrequencies et setFreqencies

--- src/Population.cpp
+++ src/Population.cpp
@@ -2,3 +2,25 @@
 
 Population::Population(const Alleles& al): popAlleles(al){}
 
+
+void Population::setFrequencies(std::vector<double> new_frequency)
+{
+	int i(0);
+	if (new_frequency.size() == getSize()) {
+		for (auto allele : popAlleles) {
+			allele.second = new_frequency[i];
+			++i;
+		}
+	} else {
+		std::cerr << "Error : parameter new_frequency doesn't have the right size N " << std::endl;
+	}
+}
+
+std::vector<double> Population::getFrequencies()
+{
+	std::vector<double> frequencies;
+	for (auto allele:popAlleles){
+		frequencies.push_back(allele.second);
+	}
+	return frequencies;
+}
