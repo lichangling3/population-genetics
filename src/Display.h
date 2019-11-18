4d8648332ef2e579e1e3f5b8292321a093f392b4
Mayer Julia Stéphanie <mayer@INTRANET.EPFL.CH@SVUbuUniv43.intranet.epfl.ch>
2019-11-18 17:28:54 +0100

    documented Display.h

--- src/Display.h
+++ src/Display.h
@@ -5,13 +5,31 @@
 #include <iostream>
 #include <fstream>
 
-
+/*! \class Display
+ * This class displays all alleles in a population with the corresponding frequencies
+ * */
 
 class Display{
 public:
+	/*! @name Initializing
+  The constructor does not need any arguments and is empty as Display does not have any attribuites.
+ */
+///@{
 	Display();
+	///@}
 	
+/*! 
+  Displays the frequencies of all the alleles of a Population. 
+  \param pop (Population): population where the alleles and their frequencies can be found.
+  \param write (std::ofstream&): ofstream where the frequencies will be written.
+ */
 	static void displayGen(Population pop, std::ofstream& write);
+	
+/*! 
+  Displays the code of all the alleles of a Population. 
+  \param pop (Population): population where the alleles and their frequencies can be found.
+  \param write (std::ofstream&): ofstream where the frequencies will be written.
+ */
 	static void displayAll(Population pop, std::ofstream& write);
 	
 };	
