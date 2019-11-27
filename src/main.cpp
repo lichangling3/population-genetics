0c4933af3c87d6a600074baa8ecab0bd41b6e183
Prunelle <prunelle.letang@gmail.com>
2019-11-27 23:22:42 +0100

    selection

--- src/main.cpp
+++ src/main.cpp
@@ -115,7 +115,7 @@ int main(int argc, char **argv) {
 					}
 				}
 			} else if(!fit.isSet()){
-				for(size_t i(0); i<number_alleles.getValue(); ++i) {
+				for(size_t i(0); i<FastaReader::size(marks.getValue(), file_name.getValue()); ++i) {
 					new_fit.push_back(0.0);
 				}
 			}
